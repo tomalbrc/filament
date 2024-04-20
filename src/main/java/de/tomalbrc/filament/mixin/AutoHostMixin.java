63d5b822411f929150079e9afbd91e7710947c8e
tomalbrc <tomalbrc@me.com>
2024-04-20 16:21:04 +0200

    rename method

--- src/main/java/de/tomalbrc/filament/mixin/AutoHostMixin.java
+++ src/main/java/de/tomalbrc/filament/mixin/AutoHostMixin.java
@@ -11,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 @Mixin(AutoHost.class)
 public class AutoHostMixin {
     @Redirect(remap = false, method = "init", at = @At(value = "FIELD", target = "Leu/pb4/polymer/autohost/impl/AutoHost;config:Leu/pb4/polymer/autohost/impl/AutoHostConfig;", opcode = Opcodes.PUTSTATIC))
-    private static void ddd(AutoHostConfig obj) {
+    private static void filament$forceAutoHost(AutoHostConfig obj) {
         obj.enabled = FilamentConfig.getInstance().forceAutohost; // we need it enabled by default..
     }
 }
