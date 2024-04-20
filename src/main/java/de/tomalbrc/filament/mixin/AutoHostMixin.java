bed45d379c9b251d54566919a9c370f210c73d04
tomalbrc <tomalbrc@me.com>
2024-04-20 16:19:45 +0200

    1.20.1 authost fix

--- src/main/java/de/tomalbrc/filament/mixin/AutoHostMixin.java
+++ src/main/java/de/tomalbrc/filament/mixin/AutoHostMixin.java
@@ -2,16 +2,16 @@ package de.tomalbrc.filament.mixin;
 
 import de.tomalbrc.filament.util.FilamentConfig;
 import eu.pb4.polymer.autohost.impl.AutoHost;
-import eu.pb4.polymer.autohost.impl.AutoHostConfig;
-import org.objectweb.asm.Opcodes;
+import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(AutoHost.class)
 public class AutoHostMixin {
-    @Redirect(remap = false, method = "init", at = @At(value = "FIELD", target = "Leu/pb4/polymer/autohost/impl/AutoHost;config:Leu/pb4/polymer/autohost/impl/AutoHostConfig;", opcode = Opcodes.PUTSTATIC))
-    private static void ddd(AutoHostConfig obj) {
-        obj.enabled = FilamentConfig.getInstance().forceAutohost; // we need it enabled by default..
+    @Inject(remap = false, method = "init", at = @At(value = "HEAD"))
+    private static void filament$forceAutoHost(MinecraftServer server, CallbackInfo ci) {
+        AutoHost.config.enabled = FilamentConfig.getInstance().forceAutohost; // we need it enabled by default..
     }
 }
