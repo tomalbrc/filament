772921e8485b6d43d187ec19a62709f5b2168759
tomalbrc <tomalbrc@me.com>
2024-06-21 21:22:02 +0200

    Load datapack configs early so that blocks can be used for worldgen

--- src/main/java/de/tomalbrc/filament/mixin/ReloadableServerResourcesMixin.java
+++ src/main/java/de/tomalbrc/filament/mixin/ReloadableServerResourcesMixin.java
@@ -1,13 +1,9 @@
 package de.tomalbrc.filament.mixin;
 
-import de.tomalbrc.filament.registry.*;
-import de.tomalbrc.filament.util.FilamentReloadUtil;
-import eu.pb4.polymer.autohost.impl.AutoHost;
+import de.tomalbrc.filament.util.Util;
 import eu.pb4.polymer.core.api.item.PolymerItemGroupUtils;
-import net.fabricmc.fabric.api.resource.SimpleSynchronousResourceReloadListener;
 import net.minecraft.commands.Commands;
 import net.minecraft.core.RegistryAccess;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.server.ReloadableServerResources;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.world.flag.FeatureFlagSet;
@@ -23,15 +19,7 @@ import java.util.concurrent.Executor;
 public class ReloadableServerResourcesMixin {
     @Inject(at = @At("HEAD"), method = "loadResources")
     private static void filament$loadResources(ResourceManager resourceManager, RegistryAccess.Frozen frozen, FeatureFlagSet featureFlagSet, Commands.CommandSelection commandSelection, int i, Executor executor, Executor executor2, CallbackInfoReturnable<CompletableFuture<ReloadableServerResources>> cir) {
-        ((RegistryUnfreezer)BuiltInRegistries.BLOCK).filament$unfreeze();
-        ((RegistryUnfreezer)BuiltInRegistries.ITEM).filament$unfreeze();
-        ((RegistryUnfreezer)BuiltInRegistries.BLOCK_ENTITY_TYPE).filament$unfreeze();
-        ((RegistryUnfreezer)BuiltInRegistries.CREATIVE_MODE_TAB).filament$unfreeze();
-
-        for (SimpleSynchronousResourceReloadListener listener : FilamentReloadUtil.getReloadListeners()) {
-            listener.onResourceManagerReload(resourceManager);
-        }
-
+        Util.loadDatapackContents(resourceManager);
         PolymerItemGroupUtils.invalidateItemGroupCache();
     }
 }
