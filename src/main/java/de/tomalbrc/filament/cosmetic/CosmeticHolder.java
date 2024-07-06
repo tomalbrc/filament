363351d610b8a86d200795afd15a7f14b50809fa
tomalbrc <tomalbrc@me.com>
2024-07-06 18:37:12 +0200

    0.5, revert cosmetic rotation changes

--- src/main/java/de/tomalbrc/filament/cosmetic/CosmeticHolder.java
+++ src/main/java/de/tomalbrc/filament/cosmetic/CosmeticHolder.java
@@ -53,8 +53,9 @@ public class CosmeticHolder extends ElementHolder {
         float yaw = this.player.getYRot();
         double i = player.getX() - this.prevX;
         double d = player.getZ() - this.prevZ;
-        float g;
-        {
+        float f = (float)(i * i + d * d);
+        float g = this.bodyYaw;
+        if (f > 0.0025f) {
             // Using internal Mojang math utils here
             float l = (float) Mth.atan2(d, i) * Mth.RAD_TO_DEG - 90.0F;
             float m = Mth.abs(Mth.wrapDegrees(yaw) - l);
