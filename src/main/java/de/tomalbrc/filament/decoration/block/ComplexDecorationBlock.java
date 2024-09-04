250c6864225919d95f25f1bdd050425da515235c
tomalbrc <tomalbrc@me.com>
2024-09-04 16:53:19 +0200

    improve pickblock for decorations

--- src/main/java/de/tomalbrc/filament/decoration/block/ComplexDecorationBlock.java
+++ src/main/java/de/tomalbrc/filament/decoration/block/ComplexDecorationBlock.java
@@ -3,6 +3,8 @@ package de.tomalbrc.filament.decoration.block;
 import de.tomalbrc.filament.decoration.block.entity.DecorationBlockEntity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
@@ -17,5 +19,12 @@ public class ComplexDecorationBlock extends DecorationBlock implements EntityBlo
     @Override
     public BlockEntity newBlockEntity(BlockPos blockPos, BlockState blockState) {
         return new DecorationBlockEntity(blockPos, blockState);
+    }
+
+    @Override
+    public ItemStack getCloneItemStack(LevelReader levelReader, BlockPos blockPos, BlockState blockState) {
+        BlockEntity blockEntity = levelReader.getBlockEntity(blockPos);
+        ItemStack stack = blockEntity instanceof DecorationBlockEntity decorationBlockEntity ? decorationBlockEntity.getItem().copyWithCount(1) : super.getCloneItemStack(levelReader, blockPos, blockState);
+        return stack;
     }
 }
