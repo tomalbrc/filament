b3bc82dc93ebab05584997b854bf3d74bc4b8451
tomalbrc <tomalbrc@me.com>
2024-10-19 15:48:42 +0200

    check for axis and direction, add field `solidOnly` to only check for solid blocks

--- src/main/java/de/tomalbrc/filament/behaviour/block/CanSurvive.java
+++ src/main/java/de/tomalbrc/filament/behaviour/block/CanSurvive.java
@@ -1,5 +1,6 @@
 package de.tomalbrc.filament.behaviour.block;
 
+import com.google.common.collect.ImmutableList;
 import de.tomalbrc.filament.api.behaviour.BlockBehaviour;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
@@ -11,6 +12,8 @@ import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.EnumProperty;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
@@ -29,18 +32,61 @@ public class CanSurvive implements BlockBehaviour<CanSurvive.Config> {
     }
 
     public boolean canSurvive(BlockState blockState, LevelReader levelReader, BlockPos blockPos) {
-        var belowState = levelReader.getBlockState(blockPos.below());
-        if (config.blocks != null && config.blocks.contains(belowState.getBlock()))
+        Direction.Axis axis = getAxisOrNull(blockState);
+        if (axis != null) {
+            return test(axis, blockPos, levelReader, blockState);
+        }
+
+        Direction direction = getFacing(blockState);
+        return test(direction, blockPos, levelReader, blockState);
+    }
+
+    private boolean test(Direction direction, BlockPos blockPos, LevelReader levelReader, BlockState blockState) {
+        var belowState = levelReader.getBlockState(blockPos.relative(direction));
+        if (this.config.blocks != null && this.config.blocks.contains(belowState.getBlock()))
             return true;
-        if (config.tags != null) {
-            for (ResourceLocation tag : config.tags) {
+        if (this.config.tags != null) {
+            for (ResourceLocation tag : this.config.tags) {
                 var tagKey = TagKey.create(Registries.BLOCK, tag);
                 if (belowState.is(tagKey))
                     return true;
             }
         }
+
+        return this.config.solidOnly && blockState.isSolid();
+    }
+
+    private boolean test(Direction.Axis axis, BlockPos blockPos, LevelReader levelReader, BlockState blockState) {
+        for (Direction direction : axis.getPlane()) {
+            if (test(direction, blockPos, levelReader, blockState)) {
+                return true;
+            }
+        }
+
         return false;
     }
+
+    private Direction.Axis getAxisOrNull(BlockState blockState) {
+        List<EnumProperty<Direction.Axis>> props2 = ImmutableList.of(BlockStateProperties.AXIS, BlockStateProperties.HORIZONTAL_AXIS);
+        for (var prop: props2) {
+            if (blockState.hasProperty(prop)) {
+                return blockState.getValue(prop);
+            }
+        }
+
+        return null;
+    }
+
+    private Direction getFacing(BlockState blockState) {
+        List<EnumProperty<Direction>> props = ImmutableList.of(BlockStateProperties.FACING, BlockStateProperties.HORIZONTAL_FACING, BlockStateProperties.VERTICAL_DIRECTION, BlockStateProperties.FACING_HOPPER);
+        for (var prop: props) {
+            if (blockState.hasProperty(prop)) {
+                return blockState.getValue(prop);
+            }
+        }
+        return Direction.DOWN;
+    }
+
 
     @Override
     public BlockState updateShape(BlockState blockState, Direction direction, BlockState blockState2, LevelAccessor levelAccessor, BlockPos blockPos, BlockPos blockPos2) {
@@ -50,5 +96,6 @@ public class CanSurvive implements BlockBehaviour<CanSurvive.Config> {
     public static class Config {
         public List<Block> blocks;
         public List<ResourceLocation> tags;
+        public boolean solidOnly = true;
     }
 }
