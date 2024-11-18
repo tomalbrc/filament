ad9883693a6f6f70cd6127582ebd7094e9155247
tomalbrc <tomalbrc@me.com>
2024-11-18 22:20:04 +0100

    improve slot check for cosmetics, allow multiple cosmetics at the same time

--- src/main/java/de/tomalbrc/filament/mixin/behaviour/cosmetic/LivingEntityMixin.java
+++ src/main/java/de/tomalbrc/filament/mixin/behaviour/cosmetic/LivingEntityMixin.java
@@ -1,6 +1,7 @@
 package de.tomalbrc.filament.mixin.behaviour.cosmetic;
 
 import com.llamalad7.mixinextras.sugar.Local;
+import de.tomalbrc.filament.behaviour.Behaviours;
 import de.tomalbrc.filament.behaviour.item.Cosmetic;
 import de.tomalbrc.filament.cosmetic.AnimatedCosmeticHolder;
 import de.tomalbrc.filament.cosmetic.CosmeticHolder;
@@ -9,6 +10,7 @@ import de.tomalbrc.filament.cosmetic.CosmeticUtil;
 import de.tomalbrc.filament.item.SimpleItem;
 import de.tomalbrc.filament.registry.ModelRegistry;
 import eu.pb4.polymer.virtualentity.api.attachment.EntityAttachment;
+import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
@@ -18,6 +20,7 @@ import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -25,13 +28,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
+import java.util.Map;
+
 @Mixin(value = LivingEntity.class)
-public class LivingEntityMixin implements CosmeticInterface {
+public abstract class LivingEntityMixin implements CosmeticInterface {
     @Unique
-    private CosmeticHolder filamentCosmeticHolder;
+    private final Map<ItemStack, CosmeticHolder> filamentCosmeticHolder = new Object2ObjectOpenHashMap<>();
 
     @Unique
-    private AnimatedCosmeticHolder filamentAnimatedCosmeticHolder;
+    private final Map<ItemStack, AnimatedCosmeticHolder> filamentAnimatedCosmeticHolder = new Object2ObjectOpenHashMap<>();
 
     @Inject(method = "getEquipmentSlotForItem", at = @At(value = "HEAD"), cancellable = true)
     private void filament$customGetEquipmentSlotForItem(ItemStack itemStack, CallbackInfoReturnable<EquipmentSlot> cir) {
@@ -43,12 +48,12 @@ public class LivingEntityMixin implements CosmeticInterface {
 
     @Inject(method = "onEquipItem", at = @At(value = "HEAD"))
     private void filament$customOnEquipItem(EquipmentSlot equipmentSlot, ItemStack itemStack, ItemStack itemStack2, CallbackInfo ci) {
-        if (equipmentSlot == EquipmentSlot.CHEST && (filamentCosmeticHolder != null || filamentAnimatedCosmeticHolder != null)) {
-            filament$destroyHolder();
+        if (CosmeticUtil.isCosmetic(itemStack)) {
+            filament$destroyHolder(itemStack);
         }
 
-        if (equipmentSlot == EquipmentSlot.CHEST && !itemStack2.isEmpty() && CosmeticUtil.isCosmetic(itemStack2) && (Object)this instanceof LivingEntity serverPlayer) {
-            filament$addHolder(serverPlayer, itemStack2.getItem(), itemStack2);
+        if (itemStack2.getItem() instanceof SimpleItem simpleItem && CosmeticUtil.isCosmetic(itemStack2) && simpleItem.get(Behaviours.COSMETIC).getConfig().slot == equipmentSlot) {
+            filament$addHolder(LivingEntity.class.cast(this), itemStack2.getItem(), itemStack2);
         }
     }
 
@@ -62,47 +67,56 @@ public class LivingEntityMixin implements CosmeticInterface {
 
     @Inject(method = "remove", at = @At(value = "HEAD"))
     private void filament$onRemove(Entity.RemovalReason removalReason, CallbackInfo ci) {
-        filament$destroyHolder();
+        for (ItemStack itemStack : LivingEntity.class.cast(this).getArmorSlots()) {
+            if (CosmeticUtil.isCosmetic(itemStack))
+                filament$destroyHolder(itemStack);
+        }
     }
 
-    @Unique public void filament$addHolder(LivingEntity livingEntity, Item simpleItem, ItemStack itemStack) {
+    @Unique
+    @Override
+    public void filament$addHolder(LivingEntity livingEntity, Item simpleItem, ItemStack itemStack) {
         Cosmetic.Config cosmeticData = CosmeticUtil.getCosmeticData(simpleItem);
 
         if (cosmeticData.model != null) {
-            if (filamentAnimatedCosmeticHolder == null) {
-                filamentAnimatedCosmeticHolder = new AnimatedCosmeticHolder(livingEntity, ModelRegistry.getModel(cosmeticData.model));
-                EntityAttachment.ofTicking(filamentAnimatedCosmeticHolder, livingEntity);
+            if (!filamentAnimatedCosmeticHolder.containsKey(itemStack)) {
+                var animatedCosmeticHolder = new AnimatedCosmeticHolder(livingEntity, ModelRegistry.getModel(cosmeticData.model));
+                EntityAttachment.ofTicking(animatedCosmeticHolder, livingEntity);
 
                 if (livingEntity instanceof ServerPlayer serverPlayer)
-                    filamentAnimatedCosmeticHolder.startWatching(serverPlayer);
+                    animatedCosmeticHolder.startWatching(serverPlayer);
 
                 if (cosmeticData.autoplay != null) {
-                    filamentAnimatedCosmeticHolder.getAnimator().playAnimation(cosmeticData.autoplay);
+                    animatedCosmeticHolder.getAnimator().playAnimation(cosmeticData.autoplay);
                 }
+                filamentAnimatedCosmeticHolder.put(itemStack, animatedCosmeticHolder);
             }
         }
         else {
-            if (filamentCosmeticHolder == null) {
-                filamentCosmeticHolder = new CosmeticHolder(livingEntity, itemStack);
-                EntityAttachment.ofTicking(filamentCosmeticHolder, livingEntity);
+            if (!filamentCosmeticHolder.containsKey(itemStack)) {
+                var cosmeticHolder = new CosmeticHolder(livingEntity, itemStack);
+                EntityAttachment.ofTicking(cosmeticHolder, livingEntity);
 
                 if (livingEntity instanceof ServerPlayer serverPlayer)
-                    filamentCosmeticHolder.startWatching(serverPlayer);
+                    cosmeticHolder.startWatching(serverPlayer);
+
+                filamentCosmeticHolder.put(itemStack, cosmeticHolder);
             }
         }
     }
 
     @Unique
-    public void filament$destroyHolder() {
-        if (filamentAnimatedCosmeticHolder != null) {
-            filamentAnimatedCosmeticHolder.getAttachment().destroy();
-            filamentAnimatedCosmeticHolder.destroy();
-            filamentAnimatedCosmeticHolder = null;
+    @Override
+    public void filament$destroyHolder(ItemStack itemStack) {
+        if (filamentAnimatedCosmeticHolder.containsKey(itemStack)) {
+            filamentAnimatedCosmeticHolder.get(itemStack).getAttachment().destroy();
+            filamentAnimatedCosmeticHolder.get(itemStack).destroy();
+            filamentAnimatedCosmeticHolder.remove(itemStack);
         }
-        if (filamentCosmeticHolder != null) {
-            filamentCosmeticHolder.getAttachment().destroy();
-            filamentCosmeticHolder.destroy();
-            filamentCosmeticHolder = null;
+        if (filamentCosmeticHolder.containsKey(itemStack)) {
+            filamentCosmeticHolder.get(itemStack).getAttachment().destroy();
+            filamentCosmeticHolder.get(itemStack).destroy();
+            filamentCosmeticHolder.remove(itemStack);
         }
     }
 }
--- src/main/java/de/tomalbrc/filament/mixin/behaviour/cosmetic/ServerPlayerMixin.java
+++ src/main/java/de/tomalbrc/filament/mixin/behaviour/cosmetic/ServerPlayerMixin.java
@@ -4,6 +4,7 @@ import de.tomalbrc.filament.cosmetic.CosmeticInterface;
 import de.tomalbrc.filament.cosmetic.CosmeticUtil;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,8 +15,22 @@ public abstract class ServerPlayerMixin implements CosmeticInterface {
     @Inject(method = "initInventoryMenu", at = @At(value = "TAIL"))
     private void filament$onInitInventoryMenu(CallbackInfo ci) {
         if ((Object)this instanceof ServerPlayer serverPlayer) {
+            var slots = serverPlayer.getArmorSlots();
+            for (ItemStack item : slots) {
+                if (!item.isEmpty() && CosmeticUtil.isCosmetic(item)) {
+                    filament$destroyHolder(item);
+                    filament$addHolder(serverPlayer, item.getItem(), item);
+                }
+            }
+        }
+    }
+
+    @Inject(method = "hasChangedDimension", at = @At("TAIL"))
+    private void filament$onChangeDimension(CallbackInfo ci) {
+        if ((Object)this instanceof ServerPlayer serverPlayer) {
             var item = serverPlayer.getItemBySlot(EquipmentSlot.CHEST);
             if (!item.isEmpty() && CosmeticUtil.isCosmetic(item)) {
+                filament$destroyHolder(item);
                 filament$addHolder(serverPlayer, item.getItem(), item);
             }
         }
