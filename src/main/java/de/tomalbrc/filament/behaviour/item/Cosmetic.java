af4046643c32133153a9f7a9977d64bc7e477bd4
tomalbrc <tomalbrc@me.com>
2024-12-14 00:11:24 +0100

    wip cosmetic masking

--- src/main/java/de/tomalbrc/filament/behaviour/item/Cosmetic.java
+++ src/main/java/de/tomalbrc/filament/behaviour/item/Cosmetic.java
@@ -1,10 +1,21 @@
 package de.tomalbrc.filament.behaviour.item;
 
 import de.tomalbrc.filament.api.behaviour.ItemBehaviour;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.Tag;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.component.CustomData;
 import org.jetbrains.annotations.NotNull;
 import org.joml.Vector3f;
+
+import java.util.List;
 
 /**
  * Cosmetics; either head or chestplate slot, can be Blockbenchmodel for chestplate slot or simple item model for either
@@ -22,11 +33,48 @@ public class Cosmetic implements ItemBehaviour<Cosmetic.Config> {
         return this.config;
     }
 
+
+    @Override
+    public void appendHoverText(ItemStack itemStack, Item.TooltipContext tooltipContext, List<Component> list, TooltipFlag tooltipFlag) {
+        if (!config.mask) return;
+
+        CustomData customData;
+        if (itemStack.has(DataComponents.CUSTOM_DATA)) {
+            var originalItem = getOriginal(tooltipContext.registries(), itemStack);
+            list.add(Component.literal("---"));
+            list.add(Component.literal("Masked Item:"));
+            list.add(Component.literal("---"));
+            list.add(originalItem.getItem().getName());
+            list.add(Component.empty());
+            originalItem.getItem().appendHoverText(originalItem, tooltipContext, list, tooltipFlag);
+        }
+    }
+
+    public static void updateMaskedData(HolderLookup.Provider lookupProvider, ItemStack original, ItemStack masker) {
+        Tag tag = original.save(lookupProvider);
+        CompoundTag compoundTag = new CompoundTag();
+        compoundTag.put("Item", tag);
+        masker.set(DataComponents.CUSTOM_DATA, CustomData.of(compoundTag));
+
+    }
+
+    public static ItemStack getOriginal(HolderLookup.Provider lookupProvider, ItemStack masker) {
+        CustomData customData = masker.get(DataComponents.CUSTOM_DATA);
+        if (customData != null) {
+            var tag = customData.copyTag();
+            return ItemStack.parseOptional(lookupProvider, tag.getCompound("Item"));
+        }
+
+        return ItemStack.EMPTY;
+    }
+
     public static class Config {
         /**
          * The equipment slot for the cosmetic
          */
         public EquipmentSlot slot;
+
+        public boolean mask = true;
 
         /**
          * The resource location of the animated model for the cosmetic.
--- src/main/java/de/tomalbrc/filament/mixin/behaviour/cosmetic/LivingEntityMixin.java
+++ src/main/java/de/tomalbrc/filament/mixin/behaviour/cosmetic/LivingEntityMixin.java
@@ -11,16 +11,22 @@ import de.tomalbrc.filament.item.SimpleItem;
 import de.tomalbrc.filament.registry.ModelRegistry;
 import eu.pb4.polymer.virtualentity.api.attachment.EntityAttachment;
 import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
+import net.fabricmc.fabric.impl.transfer.item.CursorSlotWrapper;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -31,37 +37,84 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import java.util.Map;
 
 @Mixin(value = LivingEntity.class)
-public abstract class LivingEntityMixin implements CosmeticInterface {
+public abstract class LivingEntityMixin extends Entity implements CosmeticInterface {
+    @Shadow public abstract void onItemPickup(ItemEntity itemEntity);
+
+    @Shadow protected abstract void serverAiStep();
+
+    @Shadow public abstract void remove(RemovalReason removalReason);
+
     @Unique
     private final Map<String, CosmeticHolder> filamentCosmeticHolder = new Object2ObjectOpenHashMap<>();
 
     @Unique
     private final Map<String, AnimatedCosmeticHolder> filamentAnimatedCosmeticHolder = new Object2ObjectOpenHashMap<>();
 
+    public LivingEntityMixin(EntityType<?> entityType, Level level) {
+        super(entityType, level);
+    }
+
     @Inject(method = "getEquipmentSlotForItem", at = @At(value = "HEAD"), cancellable = true)
     private void filament$customGetEquipmentSlotForItem(ItemStack itemStack, CallbackInfoReturnable<EquipmentSlot> cir) {
         Cosmetic.Config cosmetic = CosmeticUtil.getCosmeticData(itemStack);
-        if (cosmetic != null) {
+        if (cosmetic != null && cosmetic.slot != null) {
             cir.setReturnValue(cosmetic.slot);
         }
     }
 
-    @Inject(method = "onEquipItem", at = @At(value = "HEAD"))
+    @Inject(method = "onEquipItem", at = @At(value = "HEAD"), cancellable = true)
     private void filament$customOnEquipItem(EquipmentSlot equipmentSlot, ItemStack oldItemStack, ItemStack newItemStack, CallbackInfo ci) {
-        if (equipmentSlot == EquipmentSlot.HEAD) {
+        if (oldItemStack.getItem() == newItemStack.getItem()) {
             return;
         }
 
         if (oldItemStack.getItem() instanceof SimpleItem simpleItem && CosmeticUtil.isCosmetic(oldItemStack)) {
             var component = oldItemStack.get(DataComponents.EQUIPPABLE);
+            var cosmetic = simpleItem.get(Behaviours.COSMETIC);
+
+            var newIsArmor = newItemStack.getItem() instanceof ArmorItem;
+            var canMask = cosmetic != null && cosmetic.getConfig().mask;
+            if (canMask && oldItemStack.has(DataComponents.CUSTOM_DATA)) { // pop item or replace data
+                if (LivingEntity.class.cast(this) instanceof ServerPlayer serverPlayer) {
+                    var item = Cosmetic.getOriginal(this.level().registryAccess(), oldItemStack);
+                    System.out.println("OUT getOrig");
+                    if (!newIsArmor) { // pop
+                        oldItemStack.remove(DataComponents.CUSTOM_DATA);
+                        serverPlayer.addItem(item);
+                    } else { // replace data
+                        //Cosmetic.updateMaskedData(this.level().registryAccess(), newItemStack, oldItemStack);
+                        //serverPlayer.inventoryMenu.setCarried(item);
+                    }
+                }
+            }
+
+            if (canMask && newItemStack.getItem() instanceof ArmorItem) {
+                Cosmetic.updateMaskedData(this.level().registryAccess(), newItemStack, oldItemStack);
+                System.out.println("OUT update insert item with armor as cursor");
+                if (LivingEntity.class.cast(this) instanceof ServerPlayer serverPlayer) {
+                    serverPlayer.setItemSlot(equipmentSlot, ItemStack.EMPTY);
+                    serverPlayer.inventoryMenu.setCarried(oldItemStack.copy());
+                    oldItemStack.setCount(0);
+                }
+                newItemStack.setCount(0);
+            }
+
             var slot = component == null ? simpleItem.get(Behaviours.COSMETIC).getConfig().slot : component.slot();
-            if (slot == equipmentSlot) filament$destroyHolder(slot.getName());
+            if (slot == equipmentSlot && equipmentSlot != EquipmentSlot.HEAD) filament$destroyHolder(slot.getName());
         }
 
         if (newItemStack.getItem() instanceof SimpleItem simpleItem && CosmeticUtil.isCosmetic(newItemStack)) {
             var component = newItemStack.get(DataComponents.EQUIPPABLE);
-            var slot = component == null ? simpleItem.get(Behaviours.COSMETIC).getConfig().slot : component.slot();
-            if (slot == equipmentSlot) {
+            var cosmetic = simpleItem.get(Behaviours.COSMETIC);
+
+            if (cosmetic != null && cosmetic.getConfig().mask && oldItemStack.getItem() instanceof ArmorItem) {
+                System.out.println("OUT update insert item with cosmetic as cursor");
+                Cosmetic.updateMaskedData(this.level().registryAccess(), oldItemStack, newItemStack);
+                oldItemStack.setCount(0);
+            }
+
+            var slot = component == null ? cosmetic.getConfig().slot : component.slot();
+            if (slot == equipmentSlot && equipmentSlot != EquipmentSlot.HEAD) {
                 filament$destroyHolder(slot.getName());
                 filament$addHolder(LivingEntity.class.cast(this), newItemStack.getItem(), newItemStack, slot.getName());
             }
