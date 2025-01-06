4fe7e008eec7631b00dc01f4a275017953ba64f9
tomalbrc <tomalbrc@me.com>
2025-01-06 14:19:15 +0100

    add loot table support for strippable blocks
    (cherry picked from commit 7cd025b6917fd942fb037d97cfed055b896d687f)

--- src/main/java/de/tomalbrc/filament/registry/StrippableRegistry.java
+++ src/main/java/de/tomalbrc/filament/registry/StrippableRegistry.java
@@ -1,5 +1,6 @@
 package de.tomalbrc.filament.registry;
 
+import com.mojang.datafixers.util.Pair;
 import it.unimi.dsi.fastutil.objects.Reference2ObjectArrayMap;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
@@ -8,17 +9,21 @@ import net.minecraft.world.level.block.Block;
 import java.util.Map;
 
 public class StrippableRegistry {
-    private static final Map<Block, ResourceLocation> strippables = new Reference2ObjectArrayMap<>();
+    private static final Map<Block, Pair<ResourceLocation, ResourceLocation>> strippables = new Reference2ObjectArrayMap<>();
 
     public static Block get(Block block) {
-        return BuiltInRegistries.BLOCK.get(strippables.get(block));
+        return BuiltInRegistries.BLOCK.get(strippables.get(block).getFirst());
+    }
+
+    public static ResourceLocation getLootTable(Block block) {
+        return strippables.get(block).getSecond();
     }
 
     public static boolean has(Block block) {
         return strippables.containsKey(block);
     }
 
-    public static void add(Block block, ResourceLocation replacement) {
-        strippables.putIfAbsent(block, replacement);
+    public static void add(Block block, ResourceLocation replacement, ResourceLocation lootTable) {
+        strippables.putIfAbsent(block, Pair.of(replacement, lootTable));
     }
 }
