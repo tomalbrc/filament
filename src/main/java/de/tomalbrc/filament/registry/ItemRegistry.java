6a22ca59106d25bebbd36030ef705c3d0156381f
tomalbrc <tomalbrc@me.com>
2025-01-07 02:09:54 +0100

    fix for translations

--- src/main/java/de/tomalbrc/filament/registry/ItemRegistry.java
+++ src/main/java/de/tomalbrc/filament/registry/ItemRegistry.java
@@ -54,7 +54,7 @@ public class ItemRegistry {
 
         var item = ItemRegistry.registerItem(key(data.id()), (newProps) -> new SimpleItem(null, newProps, data, data.vanillaItem()), properties, data.group() != null ? data.group() : Constants.ITEM_GROUP_ID);
         BehaviourUtil.postInitItem(item, item, data.behaviour());
-        Translations.add(item, data);
+        Translations.add(item, null, data);
 
         var itemResources = data.itemResource();
         if (itemResources != null && data.itemModel() == null && itemResources.getModels() != null) {
--- src/main/java/de/tomalbrc/filament/util/Translations.java
+++ src/main/java/de/tomalbrc/filament/util/Translations.java
@@ -2,6 +2,7 @@ package de.tomalbrc.filament.util;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
+import com.mojang.datafixers.util.Pair;
 import de.tomalbrc.filament.data.Data;
 import eu.pb4.polymer.resourcepack.api.PolymerResourcePackUtils;
 import it.unimi.dsi.fastutil.objects.Object2ObjectArrayMap;
@@ -13,51 +14,36 @@ import java.util.Map;
 import java.util.Objects;
 
 public class Translations {
-    // locale => item => text
-    private static final Map<String, Map<Item, String>> translations = new Object2ObjectArrayMap<>();
-    private static final Map<String, Map<Block, String>> blockTranslations = new Object2ObjectArrayMap<>();
-
-    public static void add(Item item, Data data) {
-        if (data.translations() != null) for (Map.Entry<String, String> entry : Objects.requireNonNull(data.translations()).entrySet()) {
-            Translations.add(entry.getKey(), item, entry.getValue());
-        }
-    }
+    // locale => Pair<Item, Block> => text (x2)
+    private static final Map<String, Map<Pair<Item, Block>, String>> translations = new Object2ObjectArrayMap<>();
 
     public static void add(Item item, Block block, Data data) {
         if (data.translations() != null) {
             for (Map.Entry<String, String> entry : Objects.requireNonNull(data.translations()).entrySet()) {
-                Translations.add(entry.getKey(), item, entry.getValue());
-                Translations.add(entry.getKey(), block, entry.getValue());
+                Translations.add(entry.getKey(), Pair.of(item, block), entry.getValue());
             }
         }
     }
 
-    private static void add(String locale, Item translationId, String text) {
+    private static void add(String locale, Pair<Item, Block> translationId, String text) {
         translations.putIfAbsent(locale, new Object2ObjectArrayMap<>());
         translations.get(locale).put(translationId, text);
     }
 
-    private static void add(String locale, Block translationId, String text) {
-        blockTranslations.putIfAbsent(locale, new Object2ObjectArrayMap<>());
-        blockTranslations.get(locale).put(translationId, text);
-    }
-
     public static void init() {
         PolymerResourcePackUtils.RESOURCE_PACK_CREATION_EVENT.register(resourcePackBuilder -> {
-            for (Map.Entry<String, Map<Item, String>> entry : translations.entrySet()) {
+            for (Map.Entry<String, Map<Pair<Item, Block>, String>> entry : translations.entrySet()) {
                 JsonObject object = new JsonObject();
-                for (Map.Entry<Item, String> idTextEntry : entry.getValue().entrySet()) {
-                    object.add(idTextEntry.getKey().getDescriptionId(), new JsonPrimitive(idTextEntry.getValue()));
+                for (Map.Entry<Pair<Item, Block>, String> idTextEntry : entry.getValue().entrySet()) {
+                    var pair = idTextEntry.getKey();
+                    if (pair.getFirst() != null) {
+                        object.add(pair.getFirst().getDescriptionId(), new JsonPrimitive(idTextEntry.getValue()));
+                    }
+                    if (pair.getSecond() != null) {
+                        object.add(pair.getSecond().getDescriptionId(), new JsonPrimitive(idTextEntry.getValue()));
+                    }
                 }
-                var str = Json.GSON.toJson(object);
-                resourcePackBuilder.addData("assets/filament/lang/" + entry.getKey() + ".json", str.getBytes(StandardCharsets.UTF_8));
-            }
 
-            for (Map.Entry<String, Map<Block, String>> entry : blockTranslations.entrySet()) {
-                JsonObject object = new JsonObject();
-                for (Map.Entry<Block, String> idTextEntry : entry.getValue().entrySet()) {
-                    object.add(idTextEntry.getKey().getDescriptionId(), new JsonPrimitive(idTextEntry.getValue()));
-                }
                 var str = Json.GSON.toJson(object);
                 resourcePackBuilder.addData("assets/filament/lang/" + entry.getKey() + ".json", str.getBytes(StandardCharsets.UTF_8));
             }
