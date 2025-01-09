14a972430c28edfc91e156bbe16479a0fe664c20
tomalbrc <tomalbrc@me.com>
2025-01-09 12:38:39 +0100

    fix fishing rod behaviour
    (cherry picked from commit ce22650c0b666257f081380191b8d0a0601040d5)

---
+++ src/main/java/de/tomalbrc/filament/mixin/behaviour/fishing_rod/FishingHookMixin.java
@@ -0,0 +1,30 @@
+package de.tomalbrc.filament.mixin.behaviour.fishing_rod;
+
+import de.tomalbrc.filament.behaviour.Behaviours;
+import de.tomalbrc.filament.item.SimpleItem;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.FishingHook;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(FishingHook.class)
+public class FishingHookMixin {
+    @Inject(method = "shouldStopFishing", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/projectile/FishingHook;discard()V"), cancellable = true)
+    private void filament$customShouldStopFishing(Player player, CallbackInfoReturnable<Boolean> cir) {
+        ItemStack itemStack = player.getMainHandItem();
+        ItemStack itemStack2 = player.getOffhandItem();
+        if (isRod(itemStack) || isRod(itemStack2)) {
+            cir.setReturnValue(false);
+        }
+    }
+
+    @Unique
+    private boolean isRod(ItemStack itemStack) {
+        return itemStack.getItem() instanceof SimpleItem simpleItem && simpleItem.has(Behaviours.FISHING_ROD);
+    }
+}
