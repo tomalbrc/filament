f66862743bca637820dc9b0768eb1991a79345c2
tomalbrc <tomalbrc@me.com>
2025-02-02 23:09:37 +0100

    feat: execute behaviour for blocks, add support for list of commands, fix command permissions

--- src/main/java/de/tomalbrc/filament/behaviour/item/Execute.java
+++ src/main/java/de/tomalbrc/filament/behaviour/item/Execute.java
@@ -1,19 +1,27 @@
 package de.tomalbrc.filament.behaviour.item;
 
+import de.tomalbrc.filament.api.behaviour.BlockBehaviour;
 import de.tomalbrc.filament.api.behaviour.ItemBehaviour;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.stats.Stats;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public class Execute implements ItemBehaviour<Execute.Config> {
+import java.util.List;
+
+public class Execute implements ItemBehaviour<Execute.Config>, BlockBehaviour<Execute.Config> {
     private final Config config;
 
     public Execute(Config config) {
@@ -28,8 +36,13 @@ public class Execute implements ItemBehaviour<Execute.Config> {
 
     @Override
     public InteractionResult use(Item item, Level level, Player user, InteractionHand hand) {
-        if (this.config.command != null && user.getServer() != null) {
-            user.getServer().getCommands().performPrefixedCommand(user.createCommandSourceStackForNameResolution((ServerLevel) user.level()).withMaximumPermission(4).withEntity(user).withPosition(user.position()).withRotation(user.getRotationVector()).withLevel((ServerLevel) user.level()), this.config.command);
+        var cmds = commands();
+
+        if (cmds != null && user.getServer() != null && user instanceof ServerPlayer serverPlayer) {
+            for (String cmd : cmds) {
+                user.getServer().getCommands().performPrefixedCommand(
+                        serverPlayer.createCommandSourceStack().withSource(user.getServer()).withMaximumPermission(4), cmd);
+            }
 
             user.awardStat(Stats.ITEM_USED.get(item));
 
@@ -47,10 +60,53 @@ public class Execute implements ItemBehaviour<Execute.Config> {
         return ItemBehaviour.super.use(item, level, user, hand);
     }
 
+    @Override
+    public InteractionResult useWithoutItem(BlockState blockState, Level level, BlockPos blockPos, Player player, BlockHitResult blockHitResult) {
+        if (player instanceof ServerPlayer serverPlayer) {
+            runCommandBlock(serverPlayer, blockPos);
+            return InteractionResult.SUCCESS_SERVER;
+        }
+        return BlockBehaviour.super.useWithoutItem(blockState, level, blockPos, player, blockHitResult);
+    }
+
+    @Override
+    public @Nullable InteractionResult useItemOn(ItemStack itemStack, BlockState blockState, Level level, BlockPos blockPos, Player player, InteractionHand interactionHand, BlockHitResult blockHitResult) {
+        if (player instanceof ServerPlayer serverPlayer) {
+            runCommandBlock(serverPlayer, blockPos);
+            return InteractionResult.SUCCESS_SERVER;
+        }
+        return BlockBehaviour.super.useItemOn(itemStack, blockState, level, blockPos, player, interactionHand, blockHitResult);
+    }
+
+    private void runCommandBlock(ServerPlayer user, BlockPos blockPos) {
+        var cmds = commands();
+        if (cmds != null && user.getServer() != null) {
+            for (String cmd : cmds) {
+                user.getServer().getCommands().performPrefixedCommand(
+                        user.createCommandSourceStack().withSource(user.getServer()).withMaximumPermission(4), cmd);
+            }
+            if (this.config.sound != null) {
+                var sound = this.config.sound;
+                user.serverLevel().playSound(null, user, BuiltInRegistries.SOUND_EVENT.get(sound).orElseThrow().value(), SoundSource.NEUTRAL, 1.0F, 1.0F);
+            }
+
+            if (this.config.consumes) {
+                user.serverLevel().destroyBlock(blockPos, config.dropBlock, user);
+            }
+        }
+    }
+
+    private List<String> commands() {
+        return config.commands == null ? this.config.command == null ? null : List.of(this.config.command) : config.commands;
+    }
+
     public static class Config {
         public boolean consumes;
 
         public String command;
+        public List<String> commands;
+
+        public boolean dropBlock = false;
 
         public ResourceLocation sound;
     }
