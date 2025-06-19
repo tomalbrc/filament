3e71199af95575b4b10113a849fd6bb0f870c591
tomalbrc <tomalbrc@me.com>
2025-06-19 11:17:29 +0200

    refactor: move execute block behaviours into their respective package

--- src/main/java/de/tomalbrc/filament/behaviour/block/ExecuteAttackBlock.java
+++ src/main/java/de/tomalbrc/filament/behaviour/block/ExecuteAttackBlock.java
@@ -1,4 +1,4 @@
-package de.tomalbrc.filament.behaviour.item;
+package de.tomalbrc.filament.behaviour.block;
 
 import de.tomalbrc.filament.api.behaviour.BlockBehaviour;
 import net.minecraft.core.BlockPos;
@@ -6,47 +6,29 @@ import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundSource;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
-public class ExecuteInteractBlock implements BlockBehaviour<ExecuteInteractBlock.Config> {
+public class ExecuteAttackBlock implements BlockBehaviour<ExecuteAttackBlock.Config> {
     private final Config config;
 
-    public ExecuteInteractBlock(Config config) {
+    public ExecuteAttackBlock(Config config) {
         this.config = config;
     }
 
     @Override
     @NotNull
-    public ExecuteInteractBlock.Config getConfig() {
+    public ExecuteAttackBlock.Config getConfig() {
         return config;
     }
 
     @Override
-    public InteractionResult useWithoutItem(BlockState blockState, Level level, BlockPos blockPos, Player player, BlockHitResult blockHitResult) {
-        if (player instanceof ServerPlayer serverPlayer) {
-            runCommandBlock(serverPlayer, blockPos);
-            return InteractionResult.SUCCESS_SERVER;
-        }
-        return BlockBehaviour.super.useWithoutItem(blockState, level, blockPos, player, blockHitResult);
-    }
-
-    @Override
-    public @Nullable InteractionResult useItemOn(ItemStack itemStack, BlockState blockState, Level level, BlockPos blockPos, Player player, InteractionHand interactionHand, BlockHitResult blockHitResult) {
-        if (player instanceof ServerPlayer serverPlayer) {
-            runCommandBlock(serverPlayer, blockPos);
-            return InteractionResult.SUCCESS_SERVER;
-        }
-        return BlockBehaviour.super.useItemOn(itemStack, blockState, level, blockPos, player, interactionHand, blockHitResult);
+    public void attack(BlockState blockState, Level level, BlockPos blockPos, Player player) {
+        if (player instanceof ServerPlayer serverPlayer) runCommandBlock(serverPlayer, blockPos);
     }
 
     public void runCommandBlock(ServerPlayer user, BlockPos blockPos) {
@@ -59,6 +41,7 @@ public class ExecuteInteractBlock implements BlockBehaviour<ExecuteInteractBlock
 
                 user.getServer().getCommands().performPrefixedCommand(css, cmd);
             }
+
             if (this.config.sound != null) {
                 var sound = this.config.sound;
                 user.level().playSound(null, user, BuiltInRegistries.SOUND_EVENT.getValue(sound), SoundSource.NEUTRAL, 1.0F, 1.0F);
