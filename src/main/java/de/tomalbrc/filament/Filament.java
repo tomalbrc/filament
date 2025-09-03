646d5e10e797790366fbc2b87297b024e3ba4c85
tomalbrc <tomalbrc@me.com>
2025-09-03 14:24:03 +0200

    remove nexo importer

--- src/main/java/de/tomalbrc/filament/Filament.java
+++ src/main/java/de/tomalbrc/filament/Filament.java
@@ -46,8 +46,6 @@ public class Filament implements ModInitializer {
         Translations.registerEventHandler();
         EntityRegistry.register();
 
-        NexoImporter.importAll();
-
         if (FilamentConfig.getInstance().commands) {
             CommandRegistrationCallback.EVENT.register((dispatcher, context, selection) -> FilamentCommand.register(dispatcher));
         }
