671fabbb81256de3bf57114fe25783c15959f7e9
tomalbrc <tomalbrc@me.com>
2025-10-07 17:41:14 +0200

    feat: ignore_entity block behaviour
    (cherry picked from commit ff29bccd9ae188336d3cb39917f6d3cdb90c1a97)

---
+++ src/main/java/de/tomalbrc/filament/behaviour/block/IgniteEntity.java
@@ -0,0 +1,35 @@
+package de.tomalbrc.filament.behaviour.block;
+
+import de.tomalbrc.filament.api.behaviour.BlockBehaviourWithEntity;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.InsideBlockEffectApplier;
+import net.minecraft.world.entity.InsideBlockEffectType;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.NotNull;
+
+public class IgniteEntity implements BlockBehaviourWithEntity<IgniteEntity.Config> {
+    private final Config config;
+
+    public IgniteEntity(Config config) {
+        this.config = config;
+    }
+
+    @Override
+    @NotNull
+    public IgniteEntity.Config getConfig() {
+        return this.config;
+    }
+
+    @Override
+    public void entityInside(BlockState blockState, Level level, BlockPos blockPos, Entity entity, InsideBlockEffectApplier insideBlockEffectApplier) {
+        insideBlockEffectApplier.apply(InsideBlockEffectType.CLEAR_FREEZE);
+        insideBlockEffectApplier.apply(InsideBlockEffectType.FIRE_IGNITE);
+        insideBlockEffectApplier.runAfter(InsideBlockEffectType.FIRE_IGNITE, entityx -> entityx.hurt(entityx.level().damageSources().inFire(), this.config.fireDamage));
+    }
+
+    public static class Config {
+        float fireDamage = 1;
+    }
+}
