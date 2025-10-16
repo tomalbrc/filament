a7417e1990ca06b82c664f9fd48d0a08395735a4
tomalbrc <tomalbrc@me.com>
2025-10-16 21:30:32 +0200

    feat: button and lever block behaviours
    (cherry picked from commit 15d3cf35c03f4388ab8f2efd136ea0bde5778cbd)

---
+++ src/main/java/de/tomalbrc/filament/behaviour/block/Button.java
@@ -0,0 +1,152 @@
+package de.tomalbrc.filament.behaviour.block;
+
+import de.tomalbrc.filament.api.behaviour.BlockBehaviour;
+import de.tomalbrc.filament.data.properties.BlockStateMappedProperty;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.InsideBlockEffectApplier;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.AbstractArrow;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Explosion;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.ButtonBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraft.world.level.redstone.ExperimentalRedstoneUtils;
+import net.minecraft.world.level.redstone.Orientation;
+import net.minecraft.world.phys.BlockHitResult;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.function.BiConsumer;
+
+public class Button implements BlockBehaviour<Button.Config> {
+    private final Config config;
+
+    public Button(Config config) {
+        this.config = config;
+    }
+
+    @Override
+    @NotNull
+    public Button.Config getConfig() {
+        return this.config;
+    }
+
+    @Override
+    public InteractionResult useWithoutItem(BlockState state, Level level, BlockPos pos, Player player, BlockHitResult hitResult) {
+        if (state.getValue(ButtonBlock.POWERED)) {
+            return InteractionResult.CONSUME;
+        } else {
+            this.press(state, level, pos, player);
+            return InteractionResult.SUCCESS;
+        }
+    }
+
+    @Override
+    public void onExplosionHit(BlockState state, ServerLevel level, BlockPos pos, Explosion explosion, BiConsumer<ItemStack, BlockPos> dropConsumer) {
+        if (explosion.canTriggerBlocks() && !(Boolean)state.getValue(ButtonBlock.POWERED)) {
+            this.press(state, level, pos, null);
+        }
+    }
+
+    public void press(BlockState state, Level level, BlockPos pos, @Nullable Player player) {
+        level.setBlock(pos, state.setValue(ButtonBlock.POWERED, true), Block.UPDATE_ALL);
+        this.updateNeighbours(state, level, pos);
+        level.scheduleTick(pos, state.getBlock(), this.config.ticksToStayPressed.getValue(state));
+        this.playSound(level, pos, true);
+        level.gameEvent(player, GameEvent.BLOCK_ACTIVATE, pos);
+    }
+
+    protected void playSound(LevelAccessor level, BlockPos pos, boolean on) {
+        level.playSound(null, pos, this.getSound(on), SoundSource.BLOCKS);
+    }
+
+    protected SoundEvent getSound(boolean on) {
+        return on ? SoundEvent.createVariableRangeEvent(this.config.clickOnSound) : SoundEvent.createVariableRangeEvent(this.config.clickOffSound);
+    }
+
+    @Override
+    public void affectNeighborsAfterRemoval(BlockState state, ServerLevel level, BlockPos pos, boolean movedByPiston) {
+        if (!movedByPiston && state.getValue(ButtonBlock.POWERED)) {
+            this.updateNeighbours(state, level, pos);
+        }
+    }
+
+    @Override
+    public int getSignal(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
+        return state.getValue(ButtonBlock.POWERED) ? this.config.powerlevel.getValue(state) : 0;
+    }
+
+    @Override
+    public int getDirectSignal(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
+        return state.getValue(ButtonBlock.POWERED) && Lever.getConnectedDirection(state) == direction ? this.config.powerlevel.getValue(state) : 0;
+    }
+
+    @Override
+    public boolean isSignalSource(BlockState state) {
+        return true;
+    }
+
+    @Override
+    public void tick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
+        if (state.getValue(ButtonBlock.POWERED)) {
+            this.pressCheck(state, level, pos);
+        }
+    }
+
+    @Override
+    public void entityInside(BlockState state, Level level, BlockPos pos, Entity entity, InsideBlockEffectApplier effectApplier) {
+        if (!level.isClientSide() && this.config.canBeActivatedByArrows.getValue(state) && !(Boolean)state.getValue(ButtonBlock.POWERED)) {
+            this.pressCheck(state, level, pos);
+        }
+    }
+
+    protected void pressCheck(BlockState state, Level level, BlockPos pos) {
+        AbstractArrow abstractArrow = this.config.canBeActivatedByArrows.getValue(state) ? level.getEntitiesOfClass(AbstractArrow.class, state.getShape(level, pos).bounds().move(pos)).stream().findFirst().orElse(null) : null;
+        boolean hasArrowInside = abstractArrow != null;
+        boolean isPowered = state.getValue(ButtonBlock.POWERED);
+        if (hasArrowInside != isPowered) {
+            level.setBlock(pos, state.setValue(ButtonBlock.POWERED, hasArrowInside), Block.UPDATE_ALL);
+            this.updateNeighbours(state, level, pos);
+            this.playSound(level, pos, hasArrowInside);
+            level.gameEvent(abstractArrow, hasArrowInside ? GameEvent.BLOCK_ACTIVATE : GameEvent.BLOCK_DEACTIVATE, pos);
+        }
+
+        if (hasArrowInside) {
+            level.scheduleTick(pos, state.getBlock(), this.config.ticksToStayPressed.getValue(state));
+        }
+    }
+
+    private void updateNeighbours(BlockState state, Level level, BlockPos pos) {
+        Direction direction = Lever.getConnectedDirection(state).getOpposite();
+        Orientation orientation = ExperimentalRedstoneUtils.initialOrientation(level, direction, direction.getAxis().isHorizontal() ? Direction.UP : state.getValue(ButtonBlock.FACING));
+        level.updateNeighborsAt(pos, state.getBlock(), orientation);
+        level.updateNeighborsAt(pos.relative(direction), state.getBlock(), orientation);
+    }
+
+    public void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(ButtonBlock.FACING, ButtonBlock.POWERED, ButtonBlock.FACE);
+    }
+
+    public static class Config {
+        public BlockStateMappedProperty<Integer> powerlevel = BlockStateMappedProperty.of(15);
+        public BlockStateMappedProperty<Integer> ticksToStayPressed = BlockStateMappedProperty.of(100);
+        public BlockStateMappedProperty<Boolean> canBeActivatedByArrows = BlockStateMappedProperty.of(true);
+        public ResourceLocation clickOnSound = SoundEvents.WOODEN_BUTTON_CLICK_ON.location();
+        public ResourceLocation clickOffSound = SoundEvents.WOODEN_BUTTON_CLICK_OFF.location();
+    }
+}
---
+++ src/main/java/de/tomalbrc/filament/behaviour/block/Lever.java
@@ -0,0 +1,119 @@
+package de.tomalbrc.filament.behaviour.block;
+
+import de.tomalbrc.filament.api.behaviour.BlockBehaviour;
+import de.tomalbrc.filament.data.properties.BlockStateMappedProperty;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Explosion;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.LeverBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraft.world.level.redstone.ExperimentalRedstoneUtils;
+import net.minecraft.world.level.redstone.Orientation;
+import net.minecraft.world.phys.BlockHitResult;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.function.BiConsumer;
+
+public class Lever implements BlockBehaviour<Lever.Config> {
+    private final Config config;
+
+    public Lever(Config config) {
+        this.config = config;
+    }
+
+    @Override
+    @NotNull
+    public Lever.Config getConfig() {
+        return this.config;
+    }
+
+    @Override
+    public InteractionResult useWithoutItem(BlockState blockState, Level level, BlockPos blockPos, Player player, BlockHitResult blockHitResult) {
+        this.cycle(blockState, level, blockPos);
+        return InteractionResult.SUCCESS;
+    }
+
+    public void onExplosionHit(BlockState blockState, ServerLevel serverLevel, BlockPos blockPos, Explosion explosion, BiConsumer<ItemStack, BlockPos> biConsumer) {
+        if (explosion.canTriggerBlocks()) {
+            this.cycle(blockState, serverLevel, blockPos);
+        }
+    }
+
+    private void cycle(BlockState blockState, Level level, BlockPos blockPos) {
+        blockState = blockState.cycle(LeverBlock.POWERED);
+        level.setBlock(blockPos, blockState, Block.UPDATE_ALL);
+        this.updateNeighbours(blockState, level, blockPos);
+        playSound(null, level, blockPos, blockState);
+        level.gameEvent(null, blockState.getValue(LeverBlock.POWERED) ? GameEvent.BLOCK_ACTIVATE : GameEvent.BLOCK_DEACTIVATE, blockPos);
+    }
+
+    protected void playSound(@Nullable Player player, LevelAccessor levelAccessor, BlockPos blockPos, BlockState blockState) {
+        float pitch = config.pitch.getOrDefault(blockState, blockState.getValue(LeverBlock.POWERED) ? 0.6F : 0.5F);
+        levelAccessor.playSound(player, blockPos, SoundEvent.createVariableRangeEvent(config.sound.getValue(blockState)), SoundSource.BLOCKS, config.volume.getValue(blockState), pitch);
+    }
+
+    @Override
+    public void affectNeighborsAfterRemoval(BlockState blockState, ServerLevel serverLevel, BlockPos blockPos, boolean bl) {
+        if (!bl && blockState.getValue(LeverBlock.POWERED)) {
+            this.updateNeighbours(blockState, serverLevel, blockPos);
+        }
+
+    }
+
+    @Override
+    public int getSignal(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, Direction direction) {
+        return blockState.getValue(LeverBlock.POWERED) ? config.powerlevel.getValue(blockState) : 0;
+    }
+
+    @Override
+    public int getDirectSignal(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, Direction direction) {
+        return blockState.getValue(LeverBlock.POWERED) && getConnectedDirection(blockState) == direction ? config.powerlevel.getValue(blockState) : 0;
+    }
+
+    @Override
+    public boolean isSignalSource(BlockState blockState) {
+        return true;
+    }
+
+    private void updateNeighbours(BlockState blockState, Level level, BlockPos blockPos) {
+        Direction direction = getConnectedDirection(blockState).getOpposite();
+        Orientation orientation = ExperimentalRedstoneUtils.initialOrientation(level, direction, direction.getAxis().isHorizontal() ? Direction.UP : blockState.getValue(LeverBlock.FACING));
+        level.updateNeighborsAt(blockPos, blockState.getBlock(), orientation);
+        level.updateNeighborsAt(blockPos.relative(direction), blockState.getBlock(), orientation);
+    }
+
+    @Override
+    public void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(LeverBlock.FACE, LeverBlock.FACING, LeverBlock.POWERED);
+    }
+
+    static Direction getConnectedDirection(BlockState state) {
+        return switch (state.getValue(LeverBlock.FACE)) {
+            case CEILING -> Direction.DOWN;
+            case FLOOR -> Direction.UP;
+            default -> state.getValue(LeverBlock.FACING);
+        };
+    }
+
+    public static class Config {
+        public BlockStateMappedProperty<Integer> powerlevel = BlockStateMappedProperty.of(15);
+        public BlockStateMappedProperty<ResourceLocation> sound = BlockStateMappedProperty.of(SoundEvents.LEVER_CLICK.location());
+        public BlockStateMappedProperty<Float> volume = BlockStateMappedProperty.of(0.3f);
+        public BlockStateMappedProperty<Float> pitch = BlockStateMappedProperty.empty();
+    }
+}
