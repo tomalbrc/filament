5df4ec3a27402ddf78b2e1533ecc8c24b84c8d68
tomalbrc <tomalbrc@me.com>
2025-10-27 21:57:33 +0100

    fix: model not showing for animated enemy mobs

--- src/main/java/de/tomalbrc/filament/entity/AnimatedFilamentEnemyMob.java
+++ src/main/java/de/tomalbrc/filament/entity/AnimatedFilamentEnemyMob.java
@@ -1,11 +1,38 @@
 package de.tomalbrc.filament.entity;
 
+import de.tomalbrc.bil.api.AnimatedEntity;
+import de.tomalbrc.bil.api.AnimatedEntityHolder;
+import de.tomalbrc.bil.core.holder.entity.living.LivingEntityHolder;
+import de.tomalbrc.filament.registry.ModelRegistry;
+import eu.pb4.polymer.virtualentity.api.attachment.EntityAttachment;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.Level;
 
-public class AnimatedFilamentEnemyMob extends FilamentEnemyMob {
+import java.util.Objects;
+
+public class AnimatedFilamentEnemyMob extends FilamentEnemyMob implements AnimatedEntity {
+    private final LivingEntityHolder<AnimatedFilamentEnemyMob> holder;
+
     public AnimatedFilamentEnemyMob(EntityType<? extends Entity> entityType, Level level) {
         super(entityType, level);
+
+        this.holder = new LivingEntityHolder<>(this, ModelRegistry.getModel(Objects.requireNonNull(data.animation()).model()));
+        EntityAttachment.ofTicking(this.holder, this);
+    }
+
+    @Override
+    public AnimatedEntityHolder getHolder() {
+        return this.holder;
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+
+        if (this.tickCount % 2 == 0) {
+            AnimationHelper.updateWalkAnimation(this, this.holder, data.animation());
+            AnimationHelper.updateHurtColor(this, this.holder);
+        }
     }
 }
