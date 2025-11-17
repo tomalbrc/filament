2a69a93fcca8887d00ffd84f035d0517223fb7b0
tomalbrc <tomalbrc@me.com>
2025-11-17 13:56:02 +0100

    fix: re-introduce old behaviours

--- src/main/java/de/tomalbrc/filament/behaviour/Behaviours.java
+++ src/main/java/de/tomalbrc/filament/behaviour/Behaviours.java
@@ -19,6 +19,7 @@ public class Behaviours {
     public static final BehaviourType<Armor, Armor.Config> ARMOR = registerBehaviour("armor", Armor.class);
     public static final BehaviourType<Enchantable, Enchantable.Config> ENCHANTABLE = registerBehaviour("enchantable", Enchantable.class);
     public static final BehaviourType<Elytra, Elytra.Config> ELYTRA = registerBehaviour("elytra", Elytra.class);
+    public static final BehaviourType<Repairable, Repairable.Config> REPAIRABLE = registerBehaviour("repairable", Repairable.class);
     public static final BehaviourType<BannerPattern, BannerPattern.Config> BANNER_PATTERN = registerBehaviour("banner_pattern", BannerPattern.class); // TODO: deprecated?
     public static final BehaviourType<Bow, Bow.Config> BOW = registerBehaviour("bow", Bow.class);
     public static final BehaviourType<Compostable, Compostable.CompostableConfig> COMPOSTABLE = registerBehaviour("compostable", Compostable.class);
