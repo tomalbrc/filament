c919c9ff1a2cbcc6af88f38783aed0dde3a1c9e8
tomalbrc <tomalbrc@me.com>
2025-11-21 15:45:40 +0100

    fix: missing mixin

---
+++ src/main/java/de/tomalbrc/filament/mixin/ServerGamePacketListenerImplMixin.java
@@ -0,0 +1,21 @@
+package de.tomalbrc.filament.mixin;
+
+import de.tomalbrc.filament.util.VirtualDestroyStage;
+import net.minecraft.server.network.ServerGamePacketListenerImpl;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+
+
+@Mixin(ServerGamePacketListenerImpl.class)
+public abstract class ServerGamePacketListenerImplMixin implements VirtualDestroyStage.ServerGamePacketListenerExtF {
+
+    @Unique
+    private final VirtualDestroyStage filament$virtualDestroyStageF = new VirtualDestroyStage();
+
+    @Override
+    public VirtualDestroyStage filament$getVirtualDestroyStage() {
+        return this.filament$virtualDestroyStageF;
+    }
+
+
+}
