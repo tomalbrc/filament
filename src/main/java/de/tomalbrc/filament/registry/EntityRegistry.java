9e907798e05ffe081d437d467215024b4ea39456
tomalbrc <tomalbrc@me.com>
2025-12-16 09:09:25 +0100

    fix: remove incorrect dfu usage

--- src/main/java/de/tomalbrc/filament/registry/EntityRegistry.java
+++ src/main/java/de/tomalbrc/filament/registry/EntityRegistry.java
@@ -130,12 +130,8 @@ public class EntityRegistry {
     }
 
     private static <T extends Entity> EntityType registerEntity(ResourceLocation id, EntityType.Builder type) {
-        Map<String, Type<?>> types = (Map<String, Type<?>>) DataFixers.getDataFixer().getSchema(DataFixUtils.makeKey(SharedConstants.getCurrentVersion().getDataVersion().getVersion())).findChoiceType(References.ENTITY).types();
-        types.put(id.toString(), types.get(BuiltInRegistries.ENTITY_TYPE.getKey(EntityType.ZOMBIE).toString()));
-
         var res = Registry.register(BuiltInRegistries.ENTITY_TYPE, id, type.build(id.toString()));
         PolymerEntityUtils.registerType(res);
-
         return res;
     }
 
